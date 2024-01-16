0bfe6725d6ebe0af823ce083055591519cc8cb97
ivan <ivan.mishakivskiy@gearheart.io>
2024-01-16 12:04:43 +0200

    fix(srttings): add .env

--- TeastApp/config/settings.py
+++ TeastApp/config/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-hefo^+ndvrd+4p8%faic75x%qbdzm-goga$hx+7wg^menfu)zt"
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-hefo^+ndvrd+4p8%faic75x%qbdzm-goga$hx+7wg^menfu)zt')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
 ALLOWED_HOSTS = []
 
